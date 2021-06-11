1ab77b17f21b540e46928502a674caa57cd5df1d
tud <tudual.quinio@protonmail.com>
2021-06-11 12:29:48 +0200

    analyse <p> done
    need to fix analyse <table>

--- transformer/src/components/Steps/Transform.ts
+++ transformer/src/components/Steps/Transform.ts
@@ -1,7 +1,7 @@
 
 class Information {
     htm: string;
-    json: { [key: string]: string | Array<{ [key: string]: string | Array<{ [key: string]: string }> }> };
+    json: { [key: string]: string | Array<{ [key: string]: string | Array<{ [key: string]: string | Array<string> }> }> };
 
     constructor(htm: string) {
         this.htm = htm;
@@ -61,9 +61,172 @@ class Information {
                 }
             }
 
-            // Delete the span
-            let content = body.substr(elem1, elem2-elem1);
-            content = content.replace("&nbsp;", "");
+            // Select the content
+            let section = body.substr(elem1, elem2-elem1);
+
+            // Clean content
+            section = section.trim()
+            while (section.indexOf("<span") != -1) {
+                let cut1 = section.indexOf("<span")
+                let cut2 =section.indexOf(">", cut1);
+                section = section.substr(0, cut1) + section.substr(cut2 + 1, section.length);
+            }
+            while (section.indexOf("</span>") != -1) {
+                section = section.replace("</span>", "");
+            }
+            while (section.indexOf("&nbsp;") != -1) {
+                section = section.replace("&nbsp;", "");
+            }
+            while (section.indexOf("�") != -1) {
+                section = section.replace("�", "");
+            }
+            while (section.indexOf("&gt;") != -1) {
+                section = section.replace("&gt;", "");
+            }
+            while (section.indexOf("</i><i>") != -1) {
+                section = section.replace("</i><i>", "");
+            }
+            while (section.indexOf("<sup><sup>") != -1) {
+                section = section.replace("<sup><sup>", "<sup>");
+            }
+            while (section.indexOf("</sup></sup>") != -1) {
+                section = section.replace("</sup></sup>", "</sup>");
+            }
+            while (section.indexOf("<b></b>") != -1) {
+                section = section.replace("<b></b>", "");
+            }
+            while (section.indexOf("<b>") != -1) {
+                section = section.replace("<b>", "<strong>");
+            }
+            while (section.indexOf("</b>") != -1) {
+                section = section.replace("</b>", "</strong>");
+            }
+
+
+            // Detect what is inside the content
+
+            // function to analyse what is inside a p html tag
+            const detect_inside_p = (j: number, start: number, content: Array<{ [key: string]: string | Array<string> }>) => {
+                let elem = section.substr(start+1, end-start-1).trim();
+                if (elem != "") {
+                    let className = section.substr(j + 9, Math.min(section.indexOf(" ", j + 9)-j-9, section.indexOf(">", j + 9)-j-9));
+                    if (className == "MsoNormal") { // p or img
+                        if (elem.substr(0, 4) == "<img") {
+                            let srcStart = elem.indexOf("src=") + 5;
+                            let srcEnd = elem.indexOf('"', srcStart);
+                            content.push({
+                                element: "img",
+                                src: elem.substr(srcStart, srcEnd-srcStart)
+                            });
+                        } else {
+                            content.push({
+                                element: "p",
+                                content: elem
+                            });
+                        }
+                    } else if (className == "MsoCaption" || className == "MsoCommentText") { // figure caption
+                        content.push({
+                            element: "fig-caption",
+                            content: elem
+                        });
+                    } else if (className == "MsoListParagraph") { // list
+                        if (content.length == 0 || content[content.length - 1].element != "list") {
+                            content.push({
+                                element: "list",
+                                content: [elem]
+                            });
+                        } else {
+                            // This is not an error "content[content.length - 1].content!" will always be a list
+                            // @ts-ignore
+                            content[content.length - 1].content!.push(elem);
+                        }
+                    } else if (className == "EHead2") { // title
+                        if (elem.substr(0, 4) == "<img") {
+                            let srcStart = elem.indexOf("src=") + 5;
+                            let srcEnd = elem.indexOf('"', srcStart);
+                            content.push({
+                                element: "img",
+                                src: elem.substr(srcStart, srcEnd-srcStart)
+                            });
+                        } else {
+                            content.push({
+                                element: "h2",
+                                content: elem
+                            });
+                        }
+                    } else {
+                        console.error(`The element ${className} isn't recognize`);
+                    };
+                }
+            }
+
+            let content = [];
+            let end = 0;
+            while (section.indexOf("<", end) != -1) {
+                let j = section.indexOf("<", end);
+                let start = section.indexOf(">", j + 1);
+
+                if (section[j + 1] == "p") { // p, img, list, title, figure caption
+
+                    end = section.indexOf("</p>", start + 1);
+                    detect_inside_p(j, start, content);
+
+                } else if (section.substr(end+1, 5) == "table") {
+                    end = section.indexOf("</table>", start + 1);
+                    const table = section.substr(start+1, end-start-1);
+                    
+                    let tr_end = 0;
+                    while (section.indexOf("<tr", tr_end) != -1) {
+                        let tr_start = table.indexOf(">", table.indexOf("<tr", start + 1) + 1) + 1;
+                        let tr_end = table.indexOf("</tr>");
+                        let tr_inside = table.substr(tr_start, tr_end).trim();
+
+                        let nb_td = table.split('</td>').length;
+                        if (nb_td == 1) {
+                            const td_start = tr_inside.indexOf(">", table.indexOf("<td", start + 1) + 1) + 1;
+                            const td_end = tr_inside.indexOf("</td>");
+                            const td_inside = tr_inside.substr(td_start, td_end).trim();
+    
+                            let p_end = 0;
+                            while (td_inside.indexOf("<p", p_end) != -1) {
+                                let p_start = td_inside.indexOf(">", table.indexOf("<p", start + 1) + 1) + 1;
+                                let start = td_inside.indexOf(">", j + 1);
+                                // No table inside a table
+                                td_inside_end = section.indexOf("</p>", start + 1);
+                                detect_inside_p(j, start, content);
+                            };
+                        }
+                    }
+                    
+                    
+                    if (nb_tr == 1 && nb_td == 1 ) {
+                        const td_start = table.indexOf(">", table.indexOf("<td", start + 1) + 1) + 1;
+                        const td_end = table.indexOf("</td>");
+                        const inside = table.substr(td_start, td_end).trim();
+
+                        let inside_end = 0;
+                        while (inside.indexOf("<", inside_end) != -1) {
+                            let j = inside.indexOf("<", inside_end);
+                            let start = inside.indexOf(">", j + 1);
+                            // No table inside a table
+                            inside_end = section.indexOf("</p>", start + 1);
+                            detect_inside_p(j, start, content);
+                        };
+                    } else {
+                        // let tr_end = 0;
+                        // while (table.indexOf("<tr ", tr_end) != -1) {
+                        //     let k = table.indexOf("<tr ", tr_end);
+                        //     let tr_start = table.indexOf(">", k + 1);
+                        // }
+                        content.push({
+                            element: "table",
+                            content: section.substr(start+1, end-start-1)
+                        });
+                    }
+                } else {
+                    end += 1;
+                }
+            }
 
             
             this.json.sections.push({
