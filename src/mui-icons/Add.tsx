e7a998caeca405b5d8117c3447cce3f43ffdbb80
tud <tudual.quinio@protonmail.com>
2021-07-13 14:45:17 +0200

    delete material/icon and import the one I used inside the project

---
+++ src/mui-icons/Add.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const Add = () => {
+  return (
+    <SvgIcon>
+      <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+    </SvgIcon>
+  );
+};
+
+export default Add;
